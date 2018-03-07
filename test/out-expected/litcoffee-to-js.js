78a616422a1ce2b54b23997458d55076dae25f74
Benjamin Lupton <b@lupton.cc>
2018-03-07 18:41:18 +0800

    coffee-script to coffeescript

--- test/out-expected/litcoffee-to-js.js
+++ test/out-expected/litcoffee-to-js.js
@@ -1,4 +1,12 @@
 (function() {
+  // The **Scope** class regulates lexical scoping within CoffeeScript. As you
+  // generate code, you create a tree of scopes in the same shape as the nested
+  // function bodies. Each scope knows about the variables declared within it,
+  // and has a reference to its parent enclosing scope. In this way, we know which
+  // variables are new and need to be declared with `var`, and which are shared
+  // with external scopes.
+
+  // Import the helpers we plan to use.
   var a;
 
   a = require('./helpers');
